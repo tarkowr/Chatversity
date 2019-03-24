8346a4466d0c3bbe7c3f80d70db40bdc32cfcebc
Connor Hansen <connorhansen@github.com>
2019-03-24 18:51:41 -0400

    Added AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { CallbackComponent } from './callback/callback.component';
-import { ProtectedComponent } from './protected/protected.component';
-import { OktaAuthGuard } from './app.guard';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './Modules/onboarding/login/login.component';
 import { ForgotComponent } from './Modules/onboarding/forgot/forgot.component';
 import { SignupComponent } from './Modules/onboarding/signup/signup.component';
 import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
 import { OktaCallbackComponent } from '@okta/okta-angular';
 import { DashboardComponent } from './Modules/home/dashboard/dashboard.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -17,7 +14,8 @@ export const routes: Routes = [
     },
     {
         path: 'dashboard',
-        component: DashboardComponent
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
     },
     { path: 'signup', component: SignupComponent },
     { path: 'login',  component: LoginComponent },
@@ -27,10 +25,5 @@ export const routes: Routes = [
     {
         path: 'implicit/callback',
         component: OktaCallbackComponent
-    },
-    {
-      path: 'protected',
-      component: ProtectedComponent,
-      canActivate: [ OktaAuthGuard ]
     },
 ];
