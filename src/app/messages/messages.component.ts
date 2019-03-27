58b0b840906e516730034f7f378e6906cbb4cc26
Connor Hansen <connorhansen@github.com>
2019-03-26 22:31:07 -0400

    Cleaned up structure

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,11 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
-import Chatkit from '@pusher/chatkit-client';
-import { Observable } from 'rxjs';
-import axios from 'axios';
+import { Component} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -13,38 +8,11 @@ import { map } from 'rxjs/operators';
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent {
 
-      messages = [];
-      users = [];
       rooms = [];
       currentUser: any;
       user_id: any;
-
-      _username = '';
-      get username(): string {
-        return this._username;
-      }
-      set username(value: string) {
-        this._username = value;
-      }
-
-      _message = '';
-      get message(): string {
-        return this._message;
-      }
-      set message(value: string) {
-        this._message = value;
-      }
-
-      sendMessage() {
-        const { message, currentUser } = this;
-        currentUser.sendMessage({
-          text: message,
-          roomId: '<your room id>',
-        });
-        this.message = '';
-      }
 
   constructor(private http: HttpClient) {
 
@@ -52,7 +20,7 @@ export class MessagesComponent implements OnInit {
     // TODO: Refactor => Add to user service
     const user_id = JSON.parse(localStorage.getItem('currentUser'))._embedded.user.id;
     this.user_id = user_id;
-    console.log(user_id);
+    console.log(`User ID: ${user_id}`);
 
     // Get Chatkit user
     this.http.post(`${environment.apiUrl}/chatkit/getuser`, {user_id}).subscribe(user => {
@@ -62,27 +30,7 @@ export class MessagesComponent implements OnInit {
     // Get all of the current user's rooms
     this.http.post(`${environment.apiUrl}/chatkit/GetUserRooms`, {user_id}).subscribe(rooms => {
       this.rooms.push(rooms);
-      console.log(rooms);
+      console.log(`Rooms: ${rooms}`);
     });
-
-    console.log(this.rooms);
   }
-
-  ngOnInit() {
-    // pipe(map(user => {
-    //   this.currentUser = user;
-    //   console.log('user: ');
-    //   console.log(user);
-    // }));
-
-    // return chatManager.connect()
-    // .then(currentUser => {
-    //   this.currentUser = currentUser;
-    //   console.log('Successful connection', currentUser);
-    // })
-    // .catch(err => {
-    //   console.log('Error on connection', err);
-    // });
-  }
-
 }
