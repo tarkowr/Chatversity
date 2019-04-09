9d00c4d26f698981cc395e1567be7126ff24e042
Noah Osterhout <noahosterhout@Noahs-MacBook-Pro.local>
2019-04-09 09:07:16 -0400

    Tried to add forgot password backend, need some help on that

--- server/routes/okta.js
+++ server/routes/okta.js
@@ -39,6 +39,43 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.post('/forgot', (req, res) => { 
+    console.log("request: ");
+    console.log(req);
+    axios.post(`https://dev-117825.okta.com/api/v1/authn`, {
+        "username": req.body.username,
+        "relayState": "localhost:4200",
+        "options": {
+          "multiOptionalFactorEnroll": false,
+          "warnBeforePasswordExpired": false
+        }
+      }, 
+      {
+        headers:{
+        "Accept":'application/json',
+        "Content-Type": 'application/json'
+        }
+    })
+    .then(user => { 
+      // TODO: Create and return Session with Session Token 
+      // TODO: https://developer.okta.com/docs/api/resources/sessions/#create-session-with-session-token
+        res.status(200).json(user.data);
+    })
+    .catch(error => {
+      // No idea if the switch statement works or if I even did it right
+      switch (error) {
+        case res.status(500):
+          res.status(500).send("<p>Error 500!</p>")
+          break;
+        case res.status(403):
+          res.status(403).send("<p>Error 403!  User not found!</p>")
+          break;
+        default:
+          res.status(500).send("<p>Error 500!</p>")
+          break;
+      };
+    });
+})
 
 
 // Get user by ID
