8041978160da74ec21593c1800eb90cc4bbe9a96
Connor Hansen <connorhansen@github.com>
2019-04-13 14:44:32 -0400

    retrieve connections on init

--- src/app/Profile-Views/profile/profile.component.ts
+++ src/app/Profile-Views/profile/profile.component.ts
@@ -9,6 +9,8 @@ import { User } from '../../Core/_models/user';
 import { UserProfile } from '../../Core/_models/profile';
 import { AuthService } from '../../Core/_services/auth.service';
 import { MessagingService } from '../../Core/_services/messaging.service';
+import { environment } from '../../../environments/environment.prod';
+import { UserService } from '../../Core/_services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -20,26 +22,41 @@ export class ProfileComponent implements OnInit {
 
   user: any;
   profile: UserProfile;
+  connections: any;
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
-    private msgService: MessagingService) { }
-  
+    private msgService: MessagingService,
+    private http: HttpClient,
+    private userService: UserService) { }
+
+
+
+
   ngOnInit() {
 
     //
     // ─── CONNECT TO CHAKIT ───────────────────────────────────────────
     //
-
     this.msgService.chatManager
     .connect()
     .then(user => {
       this.user = user;
       console.log(user); // ! TESTING ONLY
+
+      // Get users connections
+      this.userService.getConnections(user.id)
+      .toPromise()
+      .then((connections) => {
+        this.connections = connections;
+        console.log(connections);
+      });
     });
     // ─────────────────────────────────────────────────────────────────
+
+
   }
 }
