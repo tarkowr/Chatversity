36a1d56ef8c5b6e60296ac89cfb80d5b02bd0451
unknown <tarkowr@gmail.com>
2019-04-15 22:41:25 -0400

    Added settings profile nonedit view

--- src/app/Core/_models/form-validation.ts
+++ src/app/Core/_models/form-validation.ts
@@ -1,5 +1,5 @@
 export class CustomFormValidation {
     eduEmailValidation:string = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+.[e]+[d]+[u]+$';
     regularEmailValidation:string = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$';
-    passwordValidation:string = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$';
+    passwordValidation:string = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
 }
