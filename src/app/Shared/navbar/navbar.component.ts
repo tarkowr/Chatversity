4b7edc3a80b022efa707dcb6678a85d7ab06a0cb
hansenconnor <connorhnsn5596@gmail.com>
2019-04-16 20:25:13 -0400

    added notification badge

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MessagingService } from '../../Core/_services/messaging.service';
 import { AuthService } from '../../Core/_services/auth.service';
 import { Observable } from 'rxjs';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-navbar',
@@ -10,48 +11,66 @@ import { Observable } from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
 
-  incomingMessages = 0;
-  currUser: any;
-  readCursors: Object;
+  currentUser: any;
+  rooms: Array<any>;
+  roomsWithNewMessages: Array<any> = [];
+
+  constructor(private _auth: AuthService, private _messaging: MessagingService) {
+
+    this._auth.chatkitUser$.subscribe(
+      (user) => {
+
+        this.currentUser = (user != null) ? user : null;
+
+        if ((user != null) && (user.id)) {
+          this.setNotifications(user);
+        }
+      }
+    );
+  }
+
+  setNotifications(user) {
+
+    let cursor;
+    const rooms = user.rooms;
+    let i = 0;
+
+    // foreach room -> compare latest message to user cursor
+    user.rooms.forEach(room => {
+
+      // get cursor
+      cursor = user.readCursor({
+        roomId: room.id
+      });
+
+      console.log(cursor);
 
 
-  constructor(private msgService: MessagingService, private _auth: AuthService, private _messaging: MessagingService) {
+      // get latest message
+      user.fetchMultipartMessages({
+        roomId: room.id,
+        direction: 'older',
+        limit: 1,
+      })
+        .then(messages => {
 
+          // compare dates -> determine if new
+          if (new Date(messages[0].updatedAt) > new Date(cursor.updatedAt)) {
+            console.log(`New message in ${messages[0].room.name}`);
+            this.roomsWithNewMessages[i] = room;
+          }
+        })
+        .catch(err => {
+          console.log(`Error fetching messages: ${err}`);
+        });
 
-    // SUBSCRIBE TO CURRENT USER … THEN GET READ CURSORS
-    _auth.user$.subscribe(($user) => {
+        i++;
 
-      this.currUser = $user;
-
-      // GET ALL OF A USER’S READ CURSORS
-      _messaging.getReadCursorsForUser($user.id)
-        .toPromise()
-        .then((cursors => {
-          this.readCursors = cursors;
-        }));
-    });
-
-    //  Get the current user's room
-
-    // Get the router url
-
-    // only display room notifications on primary navbar if the router URL is NOT rooms
-
-    // iterate through rooms
-      // if user read cursor does not equal the
-
-    //
-
-    // How many messages?
-
-    // Room for each message?
-
-     _auth.messages$.subscribe(() => {
-      this.incomingMessages += 1;
-      // console.log(this.incomingMessages);
     });
 
   }
+
+
 
   ngOnInit() {}
 
