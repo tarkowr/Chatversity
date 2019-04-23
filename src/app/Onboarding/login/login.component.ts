1961ef821d8624581cbb41a73d44d0da798db1d7
hansenconnor <connorhnsn5596@gmail.com>
2019-04-23 12:06:39 -0400

    formatting

--- src/app/Onboarding/login/login.component.ts
+++ src/app/Onboarding/login/login.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import {AuthService} from '../../Core/_services/auth.service';
-import { CustomFormValidation } from '../../Core/_models/form-validation';
-import { MessagingService } from '../../Core/_services/messaging.service';
+import { Component, OnInit } from '@angular/core'
+import { HttpHeaders } from '@angular/common/http'
+import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import {AuthService} from '../../Core/_services/auth.service'
+import { CustomFormValidation } from '../../Core/_models/form-validation'
+import { MessagingService } from '../../Core/_services/messaging.service'
 
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
     'Authorization': 'my-auth-token'
   })
-};
+}
 
 @Component({
   selector: 'app-login',
@@ -20,11 +20,11 @@ const httpOptions = {
 })
 
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl = 'home';
-  formValidation: CustomFormValidation = new CustomFormValidation();
+  loginForm: FormGroup
+  loading = false
+  submitted = false
+  returnUrl = 'home'
+  formValidation: CustomFormValidation = new CustomFormValidation()
 
   constructor (
     private formBuilder: FormBuilder,
@@ -41,32 +41,32 @@ export class LoginComponent implements OnInit {
         Validators.pattern(this.formValidation.regularEmailValidation)]
       )],
       password: ['', Validators.required]
-  });
+  })
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.loginForm.controls; }
+  get f() { return this.loginForm.controls }
 
   //
   // ─── HANDLE LOGIN FORM ──────────────────────────────────────────────────────────
   //
     onSubmit() {
 
-      this.submitted = true;
-      this.loading = true;
+      this.submitted = true
+      this.loading = true
 
       // Stop here if form is invalid
       if (this.loginForm.invalid) {
-        this.loading = false;
-        return;
+        this.loading = false
+        return
       }
 
       // Create obj to hold formdata
-      const formData: FormData = new FormData();
+      const formData: FormData = new FormData()
 
       // Append username and password to form data
-      formData.append('username', this.loginForm.get('username').value);
-      formData.append('password', this.loginForm.get('password').value);
+      formData.append('username', this.loginForm.get('username').value)
+      formData.append('password', this.loginForm.get('password').value)
 
       this.authService.login(this.f.username.value, this.f.password.value).then(oktaUser => {
 
@@ -84,16 +84,16 @@ export class LoginComponent implements OnInit {
         //     this.authService.currentUser = chatkitUser;
         //     console.log(this.authService.currentUser);
 
-        //     this.router.navigate([this.returnUrl]);
+            this.router.navigate([this.returnUrl])
 
         // });
 
       },
       error => {
-        console.log('LOGIN ERROR:', error);
-        this.loading = false;
-        this.loginForm.setErrors( {'invalid': true} );
-      });
+        console.log('LOGIN ERROR:', error)
+        this.loading = false
+        this.loginForm.setErrors( {'invalid': true} )
+      })
     }
   // ─────────────────────────────────────────────────────────────────
 
